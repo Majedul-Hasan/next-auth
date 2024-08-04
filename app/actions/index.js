e1919513e0f24b3b4fa0ca8f806957408853e807
Majedul Hasan <hasanmajedul@gmail.com>
2024-08-04 12:15:58 +0600

    doSignIn

--- app/actions/index.js
+++ app/actions/index.js
@@ -5,3 +5,6 @@ import { signIn, signOut } from '@/auth';
 export async function doSignOut() {
   await signOut();
 }
+export async function doSignIn() {
+  await signIn('google', { callbackUrl: 'http://localhost:3000' });
+}
